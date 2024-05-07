d756fb1e2ce6c263a54b31f521b29201fe244e8a
simonejonsson <simone_jonsson@outlook.com>
2024-05-07 13:09:38 +0200

    src filer + test filer till jest-projekt

---
+++ test/stack.test.js
@@ -0,0 +1,21 @@
+const stack = require('../src/stack');
+
+//test 1 - undedined
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
+//test 2 - minst element reurnerar något annat än undefined
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
+
+//test 3 - två element returnerar det översta elementet
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
+});
